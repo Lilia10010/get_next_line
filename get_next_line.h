2d97a2cce43f32af7fc05bf345758d8a58c90ceb
Lilia Paula Neiva Da Silva <lpaula-n@c1r4p1.42sp.org.br>
2024-11-14 00:21:02 -0300

    init

---
+++ get_next_line.h
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpaula-n <lpaula-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 00:31:30 by lpaula-n          #+#    #+#             */
+/*   Updated: 2024/11/13 21:13:04 by lpaula-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>    // open()
+#include <unistd.h>   // read(), write(), close()
+#include <stdio.h>    // colocar a minha printf()
+#include <stdlib.h>   // malloc(), free()
